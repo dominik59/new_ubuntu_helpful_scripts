c263a033df69d801418d67529dfa51c1de2f73d4
dominik59 <osemka59@gmail.com>
2018-02-03 19:36:44 +0100

    Added script for touchpad gestures configurations

---
+++ touchpad_gestures_configuration.sh
@@ -0,0 +1,27 @@
+if id -nG "$USER" | grep -qw "input"; then
+    apt install xdotool wmctrl libinput-tools git -y
+    (cd /tmp && git clone http://github.com/bulletmark/libinput-gestures && cd libinput-gestures; sudo ./libinput-gestures-setup install)
+    (cd ~/.config && touch libinput-gestures.conf && cat > libinput-gestures.conf << TEXT
+# KDE: Present Windows (current desktop)
+gesture swipe up    xdotool key ctrl+F9
+
+# KDE: Present Windows (Window class)
+gesture swipe down    xdotool key ctrl+F7
+
+# KDE: Show desktop
+gesture pinch out xdotool key ctrl+F12
+
+#next desktop
+gesture swipe right xdotool key ctrl+alt+Right
+
+#previous desktop
+gesture swipe left xdotool key ctrl+alt+Left
+TEXT
+)
+    libinput-gestures-setup start
+    libinput-gestures-setup autostart
+
+else
+	sudo gpasswd -a $USER input
+    echo You have been added to input group, please logout and run this script again
+fi
